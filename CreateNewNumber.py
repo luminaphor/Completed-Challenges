e277588a732d2af668670e6ded00543051e4c304
Jocelyn <43040516+pseudoEternity@users.noreply.github.com>
2019-05-15 15:04:42 -0400

    Create CreateNewNumber.py

---
+++ CreateNewNumber.py
@@ -0,0 +1,13 @@
+#written in Python
+#takes in an input as a string, loops through each number in the string
+#converts the number to an int, adds 1 to it
+#and then converts it back to a string where it is appended to newNum
+#probably not the cleanest way to do this
+
+def getNum():
+    num=input('')
+    newNum=''
+    for i in num:
+        i=int(i)+1
+        newNum+=str(i)
+    return newNum
